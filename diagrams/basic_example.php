b2956e5bdfc602897ba9391178e48ad7081ad681
TimiTao <kunicki.tomasz@gmail.com>
2019-12-14 09:46:37 +0100

    init first integration

---
+++ diagrams/basic_example.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+use StructurizrPHP\Client\Client;
+use StructurizrPHP\Client\Credentials;
+use StructurizrPHP\Client\Infrastructure\Http\SymfonyRequestFactory;
+use StructurizrPHP\Client\UrlMap;
+use StructurizrPHP\Core\Model\Enterprise;
+use StructurizrPHP\Core\Model\Location;
+use StructurizrPHP\Core\Model\Tags;
+use StructurizrPHP\Core\View\Configuration\Shape;
+use StructurizrPHP\Core\Workspace;
+use Symfony\Component\HttpClient\Psr18Client;
+
+require __DIR__ . '/../vendor/autoload.php';
+$workspace = new Workspace(
+    $id = (string)\getenv('STRUCTURIZR_WORKSPACE_ID'),
+    $name = 'Getting Started',
+    $description = 'This is a model of my software system. by structurizr-php/structurizr-php'
+);
+$workspace->getModel()->setEnterprise(new Enterprise('Structurizr PHP'));
+$person = $workspace->getModel()->addPerson(
+    $name = 'User',
+    $description = 'A user of my software system.',
+    Location::internal()
+);
+$softwareSystem = $workspace->getModel()->addSoftwareSystem(
+    $name = 'Software System',
+    $description = 'My software system.',
+    Location::internal()
+);
+$person->usesSoftwareSystem($softwareSystem, 'Uses', 'Http');
+
+$contextView = $workspace->getViews()->createSystemContextView($softwareSystem, 'System Context', 'system01', 'An example of a System Context diagram.');
+$contextView->addAllElements();
+$contextView->setAutomaticLayout(true);
+
+$styles = $workspace->getViews()->getConfiguration()->getStyles();
+
+$styles->addElementStyle(Tags::SOFTWARE_SYSTEM)->background("#1168bd")->color('#ffffff');
+$styles->addElementStyle(Tags::PERSON)->background("#08427b")->color('#ffffff')->shape(Shape::person());
+
+$client = new Client(
+    new Credentials((string) \getenv('STRUCTURIZR_API_KEY'), (string) \getenv('STRUCTURIZR_API_SECRET')),
+    new UrlMap('https://api.structurizr.com'),
+    new Psr18Client(),
+    new SymfonyRequestFactory(),
+    (new Logger('structurizr'))->pushHandler(new StreamHandler(__DIR__ . '/../var/logs/' . basename(__FILE__) . '.log', Logger::DEBUG))
+    );
+$client->put($workspace);
