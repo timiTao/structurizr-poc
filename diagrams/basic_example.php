a65c011e181d5cd93654098d636ccbdb68ee24b1
TimiTao <kunicki.tomasz@gmail.com>
2019-12-16 16:16:32 +0100

    basic example

--- diagrams/basic_example.php
+++ diagrams/basic_example.php
@@ -10,7 +10,6 @@ use StructurizrPHP\Client\Credentials;
 use StructurizrPHP\Client\Infrastructure\Http\SymfonyRequestFactory;
 use StructurizrPHP\Client\UrlMap;
 use StructurizrPHP\Core\Model\Location;
-use StructurizrPHP\Core\Model\Relationship\InteractionStyle;
 use StructurizrPHP\Core\Workspace;
 use Symfony\Component\HttpClient\Psr18Client;
 
@@ -19,19 +18,20 @@ require __DIR__ . '/../vendor/autoload.php';
 $workspace = new Workspace((string)\getenv('STRUCTURIZR_WORKSPACE_ID'), 'Simple', 'Example model with test ');
 $model = $workspace->getModel();
 
-$documentation = $model->addSoftwareSystem('Documentation', '', Location::unspecified());
-$moduleB = $model->addSoftwareSystem('Module B', '', Location::internal());
-$moduleA = $model->addSoftwareSystem('Module A', '', Location::internal());
-$architecture = $model->addSoftwareSystem('Architecture', 'General view', Location::external());
+$user = $model->addPerson('User', '', Location::external());
+$system = $model->addSoftwareSystem('Another system', '', Location::external());
 
-$architecture->usesSoftwareSystem($moduleA, 'Uses', null, InteractionStyle::asynchronous())
-$architecture->usesSoftwareSystem($moduleB, 'Uses', null, InteractionStyle::synchronous());
+$documentation = $model->addSoftwareSystem('Documentation', '', Location::internal());
 
-$moduleA->usesSoftwareSystem($moduleB);
+$application = $model->addSoftwareSystem('Application', 'General application', Location::internal());
+$integration = $model->addSoftwareSystem('Integration', 'General integration with 3th party', Location::external());
+$user->uses($application, 'uses');
+$system->usesSoftwareSystem($application, 'uses');
+$application->usesSoftwareSystem($integration);
 
-$generalArchitectureView = $workspace->getViews()->createSystemContextView($architecture, 'architecture', 'General architecture');
-$generalArchitectureView->addAllElements();
-$generalArchitectureView->setAutomaticLayout(true);
+$applicationSystemView = $workspace->getViews()->createSystemContextView($application, 'General use', 'General architecture');
+$applicationSystemView->addAllElements();
+$applicationSystemView->setAutomaticLayout(true);
 
 //region Documentation of ADR
 $adrDirectory = __DIR__ . '/../doc/adr';
@@ -39,6 +39,7 @@ $adrToolsImporter = new AdrToolsImporter($workspace, $adrDirectory);
 $adrToolsImporter->importArchitectureDecisionRecords($documentation);
 //endregion
 
+//region Export workspace
 $client = new Client(
     new Credentials((string)\getenv('STRUCTURIZR_API_KEY'), (string)\getenv('STRUCTURIZR_API_SECRET')),
     new UrlMap('https://api.structurizr.com'),
@@ -47,3 +48,4 @@ $client = new Client(
     (new Logger('structurizr'))->pushHandler(new StreamHandler(__DIR__ . '/../var/logs/' . basename(__FILE__) . '.log', Logger::DEBUG))
 );
 $client->put($workspace);
+//endregion
